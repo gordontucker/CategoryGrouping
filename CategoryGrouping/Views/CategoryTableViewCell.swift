4ed6cea4c14b12540e86b72264320e283081b537
Gordon Tucker <gordon@thetuckerbros.com>
2016-01-28 19:32:12 -0700

    Moved files into their correct folders and started adding the table and collection views

---
+++ CategoryGrouping/Views/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  CategoryGrouping
+//
+//  Created by Gordon Tucker on 1/28/16.
+//
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CategoryGrouping/Views/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  CategoryGrouping
+//
+//  Created by Gordon Tucker on 1/28/16.
+//
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
